fb685674ccb86ba73b6048cbeed32150f4129dbb
Daniel Kim <kkdh1109@gmail.com>
2020-12-03 11:11:00 -0500

    create end pts

--- netflix-clone/src/request.js
+++ netflix-clone/src/request.js
@@ -1,2 +1,14 @@
 const API_KEY = "71287c918903884f4e6499d74b8870f0";
 
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en_US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
