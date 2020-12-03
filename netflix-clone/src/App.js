17f58df8f800c730db4bbb5f4bff56ceb5b5364d
Daniel Kim <kkdh1109@gmail.com>
2020-12-03 16:32:12 -0500

    make axios call for movies sorted by genres

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
 import './App.css';
+import Row from './components/Row/Row';
+import requests from './request';
 
 function App() {
   return (
-    <div className="App">
-  
+    <div className="app">
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rating" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
     </div>
   );
 }
