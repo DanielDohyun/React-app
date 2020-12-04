6fc1d9a6932f2137622dace5ffb274e244b2f9c9
Daniel Kim <kkdh1109@gmail.com>
2020-12-04 12:58:27 -0500

    add fade at the bottom of banner

--- netflix-clone/src/components/Banner/Banner.js
+++ netflix-clone/src/components/Banner/Banner.js
@@ -49,8 +49,9 @@ function Banner() {
                 <h1 className="banner__description">
                     {truncate(movie?.overview, 150)}
                 </h1>
-
             </div>
+
+            <div className="banner--fadeBottom"></div>
         </header>
     )
 }
