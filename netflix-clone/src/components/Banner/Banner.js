bcfe4a36c540f43caf54090e89f1ca588b29a6d6
Daniel Kim <kkdh1109@gmail.com>
2020-12-04 12:53:24 -0500

    style banner component

--- netflix-clone/src/components/Banner/Banner.js
+++ netflix-clone/src/components/Banner/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from '../../axios';
 import requests from '../../request'
+import './Banner.scss';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -19,12 +20,37 @@ function Banner() {
         fetchData();
     }, []);
 
-    console.log(movie)
+    console.log(movie);
+
+    //takes the string and number and cut the string into the length of n
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+    }
 
     return (
 
-        <header>
-            
+        <header className="banner"
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: "center center",
+            }}
+        >
+            <div className="banner__contents">
+                <h1 className="banner__title">
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+
+                <div className="banner__buttons">
+                    <button className="banner__btn">Play</button>
+                    <button className="banner__btn">My List</button>
+                </div>
+
+                <h1 className="banner__description">
+                    {truncate(movie?.overview, 150)}
+                </h1>
+
+            </div>
         </header>
     )
 }
