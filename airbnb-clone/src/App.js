91e03ae3c2fceb7ac6968adbf2dd9db7ef121676
Daniel Kim <kkdh1109@gmail.com>
2020-12-05 11:28:23 -0500

    start header comp and i material ui

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Banner from './components/Banner/Banner';
+import Cards from './components/Cards/Cards';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="app">
-   
+      <Header />
+      <Home />
+      <Banner />
+      <Cards />
+      <Footer />
+
     </div>
   );
 }
