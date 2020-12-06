f55ff7e23e8d1a01c75dbd7e3e1ef93065bcc46d
Daniel Kim <kkdh1109@gmail.com>
2020-12-06 11:19:48 -0500

    setup router for hompage and searchpage

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,16 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import SearchPage from './components/SearchPage/SearchPage';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-
-      <Footer />
-
+      <Router>
+        <Header />
+          <Switch>
+            <Route path="/search">
+              <SearchPage />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        <Footer />
+      </Router>  
     </div>
   );
 }
