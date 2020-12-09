c2fbf6fe9b9e0395ccca8b1aabdc3b0439141f85
Daniel Kim <kkdh1109@gmail.com>
2020-12-09 15:51:12 -0500

    create firebase.js and header comp

---
+++ fb-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9M1Lstl2nm5dm51N1TosFUOvJbYrJQOA",
+    authDomain: "fb-clone-cf5a3.firebaseapp.com",
+    projectId: "fb-clone-cf5a3",
+    storageBucket: "fb-clone-cf5a3.appspot.com",
+    messagingSenderId: "907058559798",
+    appId: "1:907058559798:web:0c2e76a3cb3962b98ed197",
+    measurementId: "G-C047LFPZ4E"
+  };
