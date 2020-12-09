4b83608dc032a4e35ba4ce98589212c86ccf6b8c
Daniel Kim <kkdh1109@gmail.com>
2020-12-09 15:59:42 -0500

    insert fb logo and i material-ui core

--- fb-clone/src/components/Header/Header.js
+++ fb-clone/src/components/Header/Header.js
@@ -4,7 +4,19 @@ import './Header.css';
 function Header() {
     return (
         <div className="header">
-            
+            <div className="header__left">
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/600px-Facebook_f_logo_%282019%29.svg.png"
+                    alt="fb logo"
+                />
+
+            </div>
+            <div className="header__middle">
+
+            </div>
+            <div className="header__right">
+
+            </div>
         </div>
     )
 }
