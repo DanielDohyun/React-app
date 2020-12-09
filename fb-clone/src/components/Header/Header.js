bf0e619357e52c756b9ec09dd46a6b148528260d
Daniel Kim <kkdh1109@gmail.com>
2020-12-09 16:17:28 -0500

    insert icons for header right section

--- fb-clone/src/components/Header/Header.js
+++ fb-clone/src/components/Header/Header.js
@@ -6,6 +6,11 @@ import FlagIcon from '@material-ui/icons/Flag';
 import SubscriptionOutlinedIcon from '@material-ui/icons/SubscriptionsOutlined';
 import StorefrontOutlinedIcon from '@material-ui/icons/StorefrontOutlined';
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar, IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Header() {
     return (
@@ -21,6 +26,7 @@ function Header() {
                 </div>
 
             </div>
+            
             <div className="header__middle">
                 <div className="header__option">
                     <HomeIcon fontSize="large" />
@@ -39,7 +45,25 @@ function Header() {
                 </div>
 
             </div>
+
             <div className="header__right">
+                <div className="header__info">
+                    <Avatar />
+                    <h4>Daniel</h4>
+                </div>
+
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
 
             </div>
         </div>
