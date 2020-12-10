b327fd503911f40cc434f57a7e1a8cf79ea0c1d8
Daniel Kim <kkdh1109@gmail.com>
2020-12-10 14:07:16 -0500

    import icons from Mui

--- fb-clone/src/components/Feed/Feed.js
+++ fb-clone/src/components/Feed/Feed.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MessageSender from '../MessageSender/MessageSender';
+import Post from '../Post/Post';
 import StoryReel from '../StoryReel/StoryReel';
 import './Feed.css';
 
@@ -8,6 +9,17 @@ function Feed() {
         <div className="feed">
             <StoryReel />
             <MessageSender />
+
+            <Post
+                // key={id}
+                profilePic='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9BpYNo6xw_SZV9C-pgTkRQ1Ig-QsuLGzi3A&usqp=CAU'
+                msg="It's working"
+                timestamp="This is a timestamp"
+                username="dhkim"
+                img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKeaw14glXivfLb6HQ7tnf7VASNAaLgarNSw&usqp=CAU"
+            />
+            <Post />
+            <Post />
         </div>
     )
 };
