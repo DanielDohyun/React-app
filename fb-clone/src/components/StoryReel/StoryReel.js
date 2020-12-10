6b560b40e35be6ac89a5a742844fa8d00aeef578
Daniel Kim <kkdh1109@gmail.com>
2020-12-10 12:53:28 -0500

    style storyReel comp

--- fb-clone/src/components/StoryReel/StoryReel.js
+++ fb-clone/src/components/StoryReel/StoryReel.js
@@ -5,25 +5,30 @@ import './StoryReel.css';
 function StoryReel() {
     return (
         <div className="storyReel">
-            <Story
+            <Story className="storyReel__story"
                 image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQerp0s0QysNj9U_zD2Gjk5BgerF6NirqCB8g&usqp=CAU"
                 profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9BpYNo6xw_SZV9C-pgTkRQ1Ig-QsuLGzi3A&usqp=CAU"
                 title="David"
             />
-            <Story
+            <Story className="storyReel__story"
                 image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKeaw14glXivfLb6HQ7tnf7VASNAaLgarNSw&usqp=CAU"
                 profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU3miMTPoPR10HlsqJt7wr-z_qlWla5keO5g&usqp=CAU"
                 title="Mike"
             />
-            <Story
+            <Story className="storyReel__story"
                 image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQerp0s0QysNj9U_zD2Gjk5BgerF6NirqCB8g&usqp=CAU"
                 profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9BpYNo6xw_SZV9C-pgTkRQ1Ig-QsuLGzi3A&usqp=CAU"
                 title="Michelle"
             />
-            <Story
+            <Story className="storyReel__story"
                 image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKeaw14glXivfLb6HQ7tnf7VASNAaLgarNSw&usqp=CAU"
                 profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU3miMTPoPR10HlsqJt7wr-z_qlWla5keO5g&usqp=CAU"
                 title="Adam"
+            />
+             <Story className="storyReel__story"
+                image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQerp0s0QysNj9U_zD2Gjk5BgerF6NirqCB8g&usqp=CAU"
+                profileSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9BpYNo6xw_SZV9C-pgTkRQ1Ig-QsuLGzi3A&usqp=CAU"
+                title="Michelle"
             />
             
         </div>
