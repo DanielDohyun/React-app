54bbb7ca7bb564f82a60b054aa520da18ba10930
Daniel Kim <kkdh1109@gmail.com>
2020-12-10 12:31:08 -0500

    start feed comp, crete storyReel and Story comp

--- fb-clone/src/components/Feed/Feed.js
+++ fb-clone/src/components/Feed/Feed.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import StoryReel from '../StoryReel/StoryReel';
 import './Feed.css';
 
 function Feed() {
     return (
         <div className="feed">
-            
+            <StoryReel />
         </div>
     )
 };
