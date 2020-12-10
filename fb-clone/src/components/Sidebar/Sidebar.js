bb117aec833adbaacaa32d7936ff0f80b9d34855
Daniel Kim <kkdh1109@gmail.com>
2020-12-10 12:19:16 -0500

    import icons from Mui

--- fb-clone/src/components/Sidebar/Sidebar.js
+++ fb-clone/src/components/Sidebar/Sidebar.js
@@ -1,11 +1,25 @@
+import { ExpandMoreOutlined } from '@material-ui/icons';
 import React from 'react';
 import SidebarRow from '../SidebarRow/SidebarRow';
 import './Sidebar.css';
+import PeopleIcon from '@material-ui/icons/People'
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital'
+import EmojiFlagsIcon from '@material-ui/icons/EmojiFlags'
+import ChatIcon from '@material-ui/icons/Chat'
+import StorefrontIcon from '@material-ui/icons/Storefront'
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary'
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <SidebarRow />
+            <SidebarRow src="" title="Daniel Kim" />
+            <SidebarRow Icon={LocalHospitalIcon} title="COVID-19 Information Center" />
+            <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+            <SidebarRow Icon={PeopleIcon} title="Friends" />
+            <SidebarRow Icon={ChatIcon} title="Messenger" />
+            <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+            <SidebarRow Icon={VideoLibraryIcon} title="Videos" />
+            <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace" />
         </div>
     )
 }
