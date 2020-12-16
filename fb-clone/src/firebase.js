914cd18b1e7ed3fc6967165fa22d1916e9b030c8
Daniel Kim <kkdh1109@gmail.com>
2020-12-16 11:04:10 -0500

    i firebase and setup firestore and auth using googleauth

--- fb-clone/src/firebase.js
+++ fb-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyB9M1Lstl2nm5dm51N1TosFUOvJbYrJQOA",
     authDomain: "fb-clone-cf5a3.firebaseapp.com",
@@ -6,4 +8,13 @@ const firebaseConfig = {
     messagingSenderId: "907058559798",
     appId: "1:907058559798:web:0c2e76a3cb3962b98ed197",
     measurementId: "G-C047LFPZ4E"
-  };
+};
+
+// connecting the front and backend
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
